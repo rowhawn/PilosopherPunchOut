7c4897e8972a2b1525cfd18d2c55edec6ce811d0
rowhawn <rowhawn@gmail.com>
2015-04-15 11:49:53 -0400

    added functionality to choose different speakers
    added various texts from project gutenberg, lyrics from rapgenius,
    organized into folders by author. Created functionality to allow user to
    choose two different speakers from the list of texts and compile
    multiple text files into one dictionary

--- punch.py
+++ punch.py
@@ -1,32 +1,55 @@
 from pymarkovchain import MarkovChain
+import os
 
 class TextGenerator:
-	def __init__(self, generatorName, trainString):
+	def __init__(self, generatorName, trainString, prefixLength):
 		self.generatorName = generatorName
 		self.chain = MarkovChain()
-		self.chain.generateDatabase(trainString, n=3)
+		self.chain.generateDatabase(trainString, n=prefixLength)
 		self.currState = []
-		while (len(self.currState) < 3):
-			self.currState = self.chain.generateString().split()[-4:-1]
+		while (len(self.currState) < prefixLength):
+			self.currState = self.chain.generateString().split()[-(prefixLength+1):-1]
 	
 	def get_next_word(self):
 		nextword = ""
-		try: 
-			nextword = self.chain._nextWord((self.currState[0], self.currState[1]))
-			self.currState = [self.currState[1], nextword]
+		try:
+			while nextword == "":
+				nextword = self.chain._nextWord(self.currState)
+				self.currState = self.currState[1:]
+				self.currState.append(nextword)
 		except UnicodeEncodeError:
 			print("unicode error")
 		return nextword
 		
 	def get_next_syllable(self):
 		return "nextsyllable"
-		
-text1 = open("C:\\Users\\dc9169\\Documents\\GitHub\\PilosopherPunchOut\\resources\\text\\prideandprejudice.txt").read()
-text2 = open("C:\\Users\\dc9169\\Documents\\GitHub\\PilosopherPunchOut\\resources\\text\\huckleberryfinn.txt").read()
 
-prideGen = TextGenerator("pride", text1)
-huckGen = TextGenerator("huck", text2)
-speakers = [prideGen, huckGen]
+wordsDir = os.getcwd() + "/resources/text"
+worders = [d for d in os.listdir(wordsDir) if os.path.isdir(os.path.join(wordsDir, d))]
+
+print("In one corner we have:")
+for index, worder in enumerate(worders):
+	print (str(index + 1) + ": " + worder)
+worder1 = int(input()) -1
+print("\n" + worders[worder1] + "!!")
+print("and in the other corner we have:") 
+for index, worder in enumerate(worders):
+	print (str(index + 1) + ": " + worder)
+worder2 = int(input())-1
+print("\n" + worders[worder2] + "!!")
+
+worder1text = ""
+for file in os.listdir(wordsDir + "/" + worders[worder1]):
+	worder1text = worder1text + open(wordsDir + "/" + worders[worder1] + "/" + file).read()
+	
+worder2text = ""
+for file in os.listdir(wordsDir + "/" + worders[worder2]):
+	worder2text = worder2text + open(wordsDir + "/" + worders[worder2] + "/" + file).read()
+	
+textGen1 = TextGenerator(worders[worder1], worder1text, 3)
+textGen2 = TextGenerator(worders[worder2], worder2text, 3)
+
+speakers = [textGen1, textGen2]
 while 1:
 	userInput = input()
 	try: 
