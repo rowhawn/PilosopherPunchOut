367697418d95ae3a8ce774a99eaeeb8d1459974d
rowhawn <rowhawn@gmail.com>
2015-10-06 18:56:55 +0000

    modified sleep parameters to not repeat triggers

--- punch.py
+++ punch.py
@@ -5,6 +5,11 @@ from queue import Queue
 import os
 import pyttsx
 from textgenerator import TextGenerator
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+import argparse
+import RPi.GPIO as GPIO
+import time
 
 def load_text_dir_as_string(textDir):
 	text = ""
@@ -25,16 +30,39 @@ def process(speaker, inputQueue):
 
 	while True:
 		if not inputQueue.empty():
-			if inputQueue.get() == 'quit':
+			channel = inputQueue.get()
+			if channel == 'quit':
 				break
 			syllable = speaker.get_next_syllable()
 			try:
+				msg = osc_message_builder.OscMessageBuilder(address = "/punch")
+				msg.add_arg(channel)
+				msg.add_arg(speaker.generatorName)
+				msg.add_arg(syllable)
+				msg = msg.build()
+				client.send(msg)
 				print(speaker.generatorName + ": " + syllable)
 				#ttsEngine.say(syllable)
 				#ttsEngine.runAndWait()	
 			except UnicodeEncodeError:
 				print("unicode error")
 
+#setup osc
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", default="10.0.0.6", help="The ip to listen on")
+parser.add_argument("--port", type=int, default=1337, help="the port to listen on")
+args = parser.parse_args()
+
+client = udp_client.UDPClient(args.ip, args.port)
+
+
+
+#setup GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.IN)
+GPIO.setup(25, GPIO.IN)
+
+				
 #install english hyphenator dictionary if necessary
 if not is_installed("en_US"): install("en_US")				
 				
@@ -65,16 +93,24 @@ print("\n" + worders[worder1] + " ready!")
 speaker2thread.start()
 print("\n" + worders[worder2] + " ready!")
 
+prev_input1 = 0
+prev_input2 = 0
+
 while 1:
-	userInput = input()
+	#userInput = input()
+	input1 = GPIO.input(24)
+	input2 = GPIO.input(25)
 	try: 
-		if userInput == '1':
+		if (not prev_input1) and input1:
 			inputQueue1.put('1')
-		elif userInput == '2':
+		elif (not prev_input2) and input2:
 			inputQueue2.put('2')
-		elif userInput == 'quit':
-			inputQueue1.put('quit')
-			inputQueue2.put('quit')
-			quit()
+		#elif userInput == 'quit':
+		#	inputQueue1.put('quit')
+		#	inputQueue2.put('quit')
+		#	quit()
+		prev_input1 = input1
+		prev_input2 = input2
+		time.sleep(0.05)
 	except UnicodeEncodeError:
 		print("unicode error")
